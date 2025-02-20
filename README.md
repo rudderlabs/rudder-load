759877db3eeb92f80df12170d5adc10dd4cb3c85
Anudeep <asa.anudeep4@gmail.com>
2025-02-20 11:43:02 +0530

    chore: test config file

--- README.md
+++ README.md
@@ -18,30 +18,30 @@ owning a set of keys. This way we can guarantee in-order delivery of events per 
    * it is a good idea to have enough writeKeys for the replicas since replica 0 will use writeKey 0, replica 1 will use writeKey 1, etc.
 3. Concurrency: 200 (number of go routines sending messages per replica, aka "producers")
 4. Message generators: 10
-   
+
    a. number of go routines that will generate events and send them to the **producers** (see point 3)
 5. Total users: 100000
-   
+
    a. number of unique users that will be used to generate the messages
 6. Hot user groups: 70,30
-   
+
    a. the sum of all the comma separated values must be equal to 100 (percentage)
-   
-   b. the percentage of user IDs concentration that will be used to generate the messages. 
+
+   b. the percentage of user IDs concentration that will be used to generate the messages.
       - In this case we have 100,000 total users (see point 5) and we are defining 2 hot user groups so we just divide
       100,000 by 2 which gives us 2 groups of 50k users each. The probability of a message being generated for a user
       in the first group is 70% and 30% for the second group.
 7. Event types: page,batch(10,0),batch(30,100)
-   
+
    a. the types of events that will be generated. In this case, the program will generate 3 different types of events:
       * page
       * batch with 10 pages
       * batch with 30 pages and 100 track events
 8. Hot event types: 60,25,15
-   
+
    a. the sum of all the comma separated values must be equal to 100 (percentage)
-   
-   b. the percentage of event types concentration that will be used to generate the messages. 
+
+   b. the percentage of event types concentration that will be used to generate the messages.
       - In this case we have 3 event types (see point 7) and we are defining 3 hot event types, one for each event type.
       Given `page,batch(10,0),batch(30,100)` and hot event types `60,25,15` we'll have 60% probability to get a `page`,
       25% probability to get a `batch(10,0)` and 15% probability to get a `batch(30,100)`.
@@ -63,8 +63,8 @@ The `Makefile` has 2 important variables:
 * `K8S_NAMESPACE`: the Kubernetes namespace where it should deploy the `rudder-load` service
 
 Those are the only variables that you can tune via the `Makefile`.
-Before deploying you will have to create a copy of your value file (e.g. `http_values.yaml`) and add `_copy.yaml` at the 
-end of the file name (e.g. `http_values_copy.yaml`). The `Makefile` will use the copied file. 
+Before deploying you will have to create a copy of your value file (e.g. `http_values.yaml`) and add `_copy.yaml` at the
+end of the file name (e.g. `http_values_copy.yaml`). The `Makefile` will use the copied file.
 Also, that file is ignored by git so you can add whatever you want to it.
 
 The docker image is built in the CI pipeline.
@@ -93,17 +93,31 @@ K8S_NAMESPACE=my-ns make logs
 go build -o load-runner cmd/load-runner/main.go
 ```
 
+### Create a test config file
+
+```yaml
+# artifacts/helm/<load-name>_values_copy.yaml will be used
+name: <load-name>
+namespace: <namespace>
+phases:
+  - duration: 30s
+    replicas: 1
+  - duration: 30s
+    replicas: 2
+  - duration: 30s
+    replicas: 1
+```
+
 ### Run the load runner
+
 ```shell
-./load-runner -d <duration> -n <namespace> -l <values-file-prefix>
+./load-runner -t <path-to-test-config-file>
 
 # Example
-./load-runner -d 1m -n rudder-load -l http
+./load-runner -t tests/spike.test.yaml
 ```
 
 ### Load runner flags
 
-- `-d`: duration of the load (e.g., 1h, 30m, 5s)
-- `-n`: namespace where the load runner will be deployed
-- `-l`: values file prefix
+- `-t`: path to the test config file
 - `-f`: path to the chart files (e.g., artifacts/helm)
--- cmd/load-runner/main.go
+++ cmd/load-runner/main.go
@@ -11,14 +11,23 @@ import (
 	"time"
 
 	"github.com/rudderlabs/rudder-go-kit/logger"
+	"gopkg.in/yaml.v3"
 )
 
-type config struct {
-	duration       string
-	parsedDuration time.Duration
-	namespace      string
-	loadName       string
+type cmdArgs struct {
 	chartFilesPath string
+	testFile       string
+}
+
+type testConfig struct {
+	Name      string     `yaml:"name"`
+	Namespace string     `yaml:"namespace"`
+	Phases    []runPhase `yaml:"phases"`
+}
+
+type runPhase struct {
+	Duration string `yaml:"duration"`
+	Replicas int    `yaml:"replicas"`
 }
 
 const (
@@ -39,52 +48,39 @@ func main() {
 }
 
 func run(ctx context.Context, log logger.Logger) error {
-	cfg, err := parseFlags(log)
+	cmd, err := parseFlags(log)
 	if err != nil {
 		return fmt.Errorf("invalid options: %w", err)
 	}
 
-	if err := validateInputs(cfg); err != nil {
+	testConfig, err := loadTestConfig(cmd)
+	if err != nil {
+		return fmt.Errorf("failed to load test config: %w", err)
+	}
+
+	if err := validateInputs(testConfig); err != nil {
 		return fmt.Errorf("invalid inputs: %w", err)
 	}
 
-	duration, err := parseDuration(cfg.duration)
-	if err != nil {
-		return fmt.Errorf("invalid duration: %w", err)
-	}
-	cfg.parsedDuration = duration
-
-	return runLoadTest(ctx, cfg, log)
+	return runLoadTest(ctx, cmd, testConfig, log)
 }
 
-func parseFlags(log logger.Logger) (*config, error) {
-	var cfg config
-	flag.StringVar(&cfg.duration, "d", "", "Duration to run (e.g., 1h, 30m, 5s)")
-	flag.StringVar(&cfg.namespace, "n", "", "Kubernetes namespace")
-	flag.StringVar(&cfg.loadName, "l", "", "Load scenario name")
+func parseFlags(log logger.Logger) (*cmdArgs, error) {
+	var cfg cmdArgs
 	flag.StringVar(&cfg.chartFilesPath, "f", "", "Path to the chart files (e.g., artifacts/helm)")
-
+	flag.StringVar(&cfg.testFile, "t", "", "Path to the test file (e.g., tests/rampup.test.yaml)")
 	flag.Usage = func() {
-		log.Info("Usage: %s [options]", os.Args[0])
+		log.Infof("Usage: %s [options]", os.Args[0])
 		log.Info("Options:")
 		flag.PrintDefaults()
 		log.Info("Examples:")
-		log.Info("  %s -d 1m -n test -l test-staging    # Run test-staging load for 1 minute on test namespace", os.Args[0])
+		log.Infof("  %s -t tests/spike.test.yaml    # Runs spike test", os.Args[0])
 	}
 
 	flag.Parse()
 
-	if cfg.duration == "" || cfg.namespace == "" || cfg.loadName == "" {
-		if cfg.duration == "" {
-			log.Error("Error: duration is required")
-		}
-		if cfg.namespace == "" {
-			log.Error("Error: namespace is required")
-		}
-		if cfg.loadName == "" {
-			log.Error("Error: load name is required")
-		}
-
+	if cfg.testFile == "" {
+		log.Error("Error: test file is required")
 		flag.Usage()
 		return nil, fmt.Errorf("invalid options")
 	}
@@ -92,17 +88,38 @@ func parseFlags(log logger.Logger) (*config, error) {
 	return &cfg, nil
 }
 
-func validateInputs(cfg *config) error {
-	if !regexp.MustCompile(`^[a-z0-9-]+$`).MatchString(cfg.namespace) {
+func loadTestConfig(cmd *cmdArgs) (*testConfig, error) {
+	var testConfig testConfig
+	data, err := os.Open(cmd.testFile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read test file: %w", err)
+	}
+	defer data.Close()
+
+	decoder := yaml.NewDecoder(data)
+	err = decoder.Decode(&testConfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode test file: %w", err)
+	}
+	return &testConfig, nil
+}
+
+func validateInputs(testConfig *testConfig) error {
+	if !regexp.MustCompile(`^[a-z0-9-]+$`).MatchString(testConfig.Namespace) {
 		return fmt.Errorf("namespace must contain only lowercase alphanumeric characters and '-'")
 	}
 
-	if !regexp.MustCompile(`^[a-zA-Z0-9-]+$`).MatchString(cfg.loadName) {
+	if !regexp.MustCompile(`^[a-zA-Z0-9-]+$`).MatchString(testConfig.Name) {
 		return fmt.Errorf("load name must contain only alphanumeric characters and '-'")
 	}
 
-	if !regexp.MustCompile(`^(\d+[hms])+$`).MatchString(cfg.duration) {
-		return fmt.Errorf("duration must include 'h', 'm', or 's' (e.g., '1h30m')")
+	for _, phase := range testConfig.Phases {
+		if !regexp.MustCompile(`^(\d+[hms])+$`).MatchString(phase.Duration) {
+			return fmt.Errorf("duration must include 'h', 'm', or 's' (e.g., '1h30m')")
+		}
+		if phase.Replicas <= 0 {
+			return fmt.Errorf("replicas must be greater than 0")
+		}
 	}
 
 	return nil
@@ -121,25 +138,25 @@ func parseDuration(d string) (time.Duration, error) {
 	return duration, nil
 }
 
-func runLoadTest(ctx context.Context, cfg *config, log logger.Logger) error {
-	releaseName := fmt.Sprintf("%s-%s", defaultReleaseNamePrefix, cfg.loadName)
+func runLoadTest(ctx context.Context, cfg *cmdArgs, testConfig *testConfig, log logger.Logger) error {
+	releaseName := fmt.Sprintf("%s-%s", defaultReleaseNamePrefix, testConfig.Name)
 
 	chartFilesPath := cfg.chartFilesPath
-	log.Info("Chart path before: %s", chartFilesPath)
+	log.Infof("Chart path before: %s", chartFilesPath)
 	if chartFilesPath == "" {
 		chartFilesPath = defaultChartFilesPath
 	}
-	log.Info("Chart path after: %s", chartFilesPath)
+	log.Infof("Chart path after: %s", chartFilesPath)
 
-	log.Info("Installing Helm chart for load scenario: %s", cfg.loadName)
+	log.Infof("Installing Helm chart for load scenario: %s", testConfig.Name)
 	installArgs := []string{
 		"install",
 		releaseName,
 		chartFilesPath,
-		"--namespace", cfg.namespace,
-		"--set", fmt.Sprintf("namespace=%s", cfg.namespace),
+		"--namespace", testConfig.Namespace,
+		"--set", fmt.Sprintf("namespace=%s", testConfig.Namespace),
 		"--set", fmt.Sprintf("deployment.name=%s", releaseName),
-		"--values", fmt.Sprintf("%s/%s_values_copy.yaml", chartFilesPath, cfg.loadName),
+		"--values", fmt.Sprintf("%s/%s_values_copy.yaml", chartFilesPath, testConfig.Name),
 	}
 
 	if err := runCommand(ctx, "helm", installArgs...); err != nil {
@@ -151,7 +168,7 @@ func runLoadTest(ctx context.Context, cfg *config, log logger.Logger) error {
 		uninstallArgs := []string{
 			"uninstall",
 			releaseName,
-			"--namespace", cfg.namespace,
+			"--namespace", testConfig.Namespace,
 		}
 		if err := runCommand(context.Background(), "helm", uninstallArgs...); err != nil {
 			log.Error("Error during cleanup: %v", err)
@@ -159,15 +176,37 @@ func runLoadTest(ctx context.Context, cfg *config, log logger.Logger) error {
 		log.Info("Done!")
 	}()
 
-	log.Info("Chart will run for %s", cfg.parsedDuration)
-	log.Info("To view logs, run: kubectl logs -n %s -l app=%s -f", cfg.namespace, releaseName)
+	log.Infof("To view logs, run: kubectl logs -n %s -l app=%s -f", testConfig.Namespace, releaseName)
 
-	select {
-	case <-ctx.Done():
-		return fmt.Errorf("operation cancelled by user")
-	case <-time.After(cfg.parsedDuration):
-		return nil
+	for _, phase := range testConfig.Phases {
+		duration, err := parseDuration(phase.Duration)
+		if err != nil {
+			return fmt.Errorf("invalid duration: %w", err)
+		}
+		log.Infof("Chart will run for %s", duration)
+		upgradeArgs := []string{
+			"upgrade",
+			releaseName,
+			chartFilesPath,
+			"--namespace", testConfig.Namespace,
+			"--set", fmt.Sprintf("namespace=%s", testConfig.Namespace),
+			"--set", fmt.Sprintf("deployment.name=%s", releaseName),
+			"--set", fmt.Sprintf("deployment.replicas=%d", phase.Replicas),
+			"--values", fmt.Sprintf("%s/%s_values_copy.yaml", chartFilesPath, testConfig.Name),
+		}
+		if err := runCommand(context.Background(), "helm", upgradeArgs...); err != nil {
+			log.Error("Error during upgrade: %v", err)
+		}
+
+		select {
+		case <-ctx.Done():
+			return fmt.Errorf("operation cancelled by user")
+		case <-time.After(duration):
+
+		}
 	}
+	return nil
+
 }
 
 func runCommand(ctx context.Context, name string, args ...string) error {
