ba8e03278f84a5c901c9902980d55f7982a098a4
Anudeep <asa.anudeep4@gmail.com>
2025-03-13 09:19:01 +0530

    chore: common function to parse env

--- cmd/load-runner/helm.go
+++ cmd/load-runner/helm.go
@@ -33,12 +33,7 @@ func (h *helmClient) Install(ctx context.Context, config *parser.LoadTestConfig)
 		"--values", fmt.Sprintf("%s/%s_values_copy.yaml", config.ChartFilePath, config.Name),
 	}
 
-	for key, value := range config.EnvOverrides {
-		if strings.Contains(value, ",") {
-			value = strings.ReplaceAll(value, ",", "\\,")
-		}
-		args = append(args, "--set", fmt.Sprintf("deployment.env.%s=%s", key, value))
-	}
+	args = processHelmEnvVars(args, config.EnvOverrides)
 
 	fmt.Printf("Running helm install with args: %v\n", args)
 	return h.executor.run(ctx, "helm", args...)
@@ -56,19 +51,8 @@ func (h *helmClient) Upgrade(ctx context.Context, config *parser.LoadTestConfig,
 		"--values", fmt.Sprintf("%s/%s_values_copy.yaml", config.ChartFilePath, config.Name),
 	}
 
-	for key, value := range config.EnvOverrides {
-		if strings.Contains(value, ",") {
-			value = strings.ReplaceAll(value, ",", "\\,")
-		}
-		args = append(args, "--set", fmt.Sprintf("deployment.env.%s=%s", key, value))
-	}
-
-	for key, value := range phase.EnvOverrides {
-		if strings.Contains(value, ",") {
-			value = strings.ReplaceAll(value, ",", "\\,")
-		}
-		args = append(args, "--set", fmt.Sprintf("deployment.env.%s=%s", key, value))
-	}
+	args = processHelmEnvVars(args, config.EnvOverrides)
+	args = processHelmEnvVars(args, phase.EnvOverrides)
 
 	return h.executor.run(ctx, "helm", args...)
 }
@@ -80,4 +64,14 @@ func (h *helmClient) Uninstall(config *parser.LoadTestConfig) error {
 		"--namespace", config.Namespace,
 	}
 	return h.executor.run(context.Background(), "helm", args...)
+}
+
+func processHelmEnvVars(args []string, envVars map[string]string) []string {
+	for key, value := range envVars {
+		if strings.Contains(value, ",") {
+			value = strings.ReplaceAll(value, ",", "\\,")
+		}
+		args = append(args, "--set", fmt.Sprintf("deployment.env.%s=%s", key, value))
+	}
+	return args
 }
