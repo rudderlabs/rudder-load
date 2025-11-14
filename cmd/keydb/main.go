b97fafbd905e3a16b267a24ba35f060105b375ea
Francesco Casula <fra.casula@gmail.com>
2025-11-14 15:15:22 +0100

    chore: adding maxprocs
    🔒 Scanned for secrets using gitleaks version is set by build process

--- cmd/keydb/main.go
+++ cmd/keydb/main.go
@@ -19,6 +19,7 @@ import (
 	"github.com/rudderlabs/keydb/client"
 	kitconfig "github.com/rudderlabs/rudder-go-kit/config"
 	"github.com/rudderlabs/rudder-go-kit/logger"
+	_ "github.com/rudderlabs/rudder-go-kit/maxprocs"
 	"github.com/rudderlabs/rudder-go-kit/profiler"
 	"github.com/rudderlabs/rudder-go-kit/stats"
 	svcMetric "github.com/rudderlabs/rudder-go-kit/stats/metric"
