52057ff71b6053ebd20731393c2254726eb90efd
Francesco Casula <fra.casula@gmail.com>
2025-11-14 15:05:47 +0100

    chore: adding profiler
    🔒 Scanned for secrets using gitleaks version is set by build process

--- cmd/keydb/main.go
+++ cmd/keydb/main.go
@@ -19,6 +19,7 @@ import (
 	"github.com/rudderlabs/keydb/client"
 	kitconfig "github.com/rudderlabs/rudder-go-kit/config"
 	"github.com/rudderlabs/rudder-go-kit/logger"
+	"github.com/rudderlabs/rudder-go-kit/profiler"
 	"github.com/rudderlabs/rudder-go-kit/stats"
 	svcMetric "github.com/rudderlabs/rudder-go-kit/stats/metric"
 	kitsync "github.com/rudderlabs/rudder-go-kit/sync"
@@ -119,6 +120,16 @@ func run(ctx context.Context) int {
 			log.Errorn("closing KeyDB client", obskit.Error(err))
 		}
 	}()
+
+	// Starting profiler
+	profilerDone := make(chan struct{})
+	go func() {
+		defer close(profilerDone)
+		if err := profiler.StartServer(ctx, conf.GetInt("Profiler.Port", 7777)); err != nil {
+			log.Errorn("profiler server error", obskit.Error(err))
+		}
+	}()
+	defer func() { <-profilerDone }()
 
 	// Statistics tracking
 	var (
